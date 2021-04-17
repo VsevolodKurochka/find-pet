6555d372e31442eb52fdcb98ef38f4ab17c94473
Vsevolod Kurochka <seva.kurochka@gmail.com>
2021-04-17 22:47:06 +0300

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className={'test'}>TEST</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
