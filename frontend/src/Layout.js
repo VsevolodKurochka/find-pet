22c5d4330115acdda268f74a1e1ddb835e413f4e
Vsevolod Kurochka <seva.kurochka@gmail.com>
2021-05-20 23:53:23 +0300

    [frontend] update routes, add images

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -1,26 +1,27 @@
-import {Layout, Menu, Breadcrumb, notification} from 'antd';
+import {Layout, Menu, notification} from 'antd';
 import {
     UserOutlined, LogoutOutlined
 } from '@ant-design/icons';
-import {useState, useCallback, useContext} from 'react';
+import {useCallback, useContext} from 'react';
 import Logo from './logo.png';
 import {
-    Link
+    Link,
+    useLocation
 } from "react-router-dom";
-import {useAuth} from './useAuth';
 import {AuthContext} from './AuthContext';
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 export const LayoutPage = ({children}) => {
+    let location = useLocation();
     const {isAuthenticated, logout} = useContext(AuthContext);
     const year = new Date().getFullYear();
 
     const logoutFromAccount = useCallback(() => {
         logout();
         notification.success({message: 'Успішно!', description: 'Ви вийшли з системи!'})
-    }, []);
+    }, [logout]);
 
     return (
         <Layout style={{ minHeight: '100vh' }}>
@@ -28,12 +29,11 @@ export const LayoutPage = ({children}) => {
                 <div className="logo">
                     <Link to={'/'}><img src={Logo} alt={'logo'} /></Link>
                 </div>
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1"><Link to="/">Головна</Link></Menu.Item>
-                    <Menu.Item key="3"><Link to="/catalog">Каталог</Link></Menu.Item>
-                    <Menu.Item key="2"><Link to="/about">Про нас</Link></Menu.Item>
-                    <Menu.Item key="4"><Link to="/contacts">Контакти</Link></Menu.Item>
-                    <Menu.Item key="5"><Link to="/blog">Статті</Link></Menu.Item>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[location.pathname]}>
+                    <Menu.Item key="/"><Link to="/">Головна</Link></Menu.Item>
+                    <Menu.Item key="/about"><Link to="/about">Про нас</Link></Menu.Item>
+                    <Menu.Item key="/contacts"><Link to="/contacts">Контакти</Link></Menu.Item>
+                    <Menu.Item key="/blog"><Link to="/blog">Статті</Link></Menu.Item>
                 </Menu>
             </Header>
             <Layout>
@@ -42,12 +42,13 @@ export const LayoutPage = ({children}) => {
                         mode="inline"
                         style={{ height: '100%', borderRight: 0 }}
                         defaultOpenKeys={['sub1']}
+                        defaultSelectedKeys={[location.pathname]}
                     >
                         {isAuthenticated && <>
                             <SubMenu key="sub1" icon={<UserOutlined />} title="Кабінет">
-                                <Menu.Item key="cab"><Link to="/profile">Кабінет</Link></Menu.Item>
-                                <Menu.Item key="add"><Link to="/profile/add">Додати оголошення</Link></Menu.Item>
-                                <Menu.Item key="animals"><Link to="/profile/animals">Мої тварини</Link></Menu.Item>
+                                <Menu.Item key="/profile"><Link to="/profile">Кабінет</Link></Menu.Item>
+                                <Menu.Item key="/profile/add"><Link to="/profile/add">Додати оголошення</Link></Menu.Item>
+                                <Menu.Item key="/profile/animals"><Link to="/profile/animals">Мої тварини</Link></Menu.Item>
                             </SubMenu>
                             <Menu.Item key="logout" icon={<LogoutOutlined />}><Link to="/" onClick={logoutFromAccount}>Вийти</Link></Menu.Item>
                         </> }
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,18 +5,22 @@ import RegisterPage from './pages/register';
 import LoginPage from './pages/login';
 import ProfilePage from './pages/profile';
 import PrivateRoute from './PrivateRoute';
-import ProfileAddAnimalPage from './pages/profile/ProfileAddAnimalPage';
+import ProfileAddAnimalPage from './pages/profile/add';
+import ProfileAnimalsPage from './pages/profile/animals';
+import AboutPage from './pages/about';
+import ContactsPage from './pages/contacts';
+import BlogPage from './pages/blog';
 
 export const useRoutes = () => {
     return (
         <Switch>
-            {/*<Route path="/login" render={() => <LoginPage isAuthenticated={isAuthenticated} />} />
-            <Route path="/register" render={() => <RegisterPage isAuthenticated={isAuthenticated} />} />*/}
-            {/*<PrivateRoute isAuthenticated={isAuthenticated} path="/admin" redirectPath="/login" component={AdminPage} />*/}
             <Route path="/login" component={LoginPage} />} />
             <Route path="/register" component={RegisterPage} />} />
+            <Route path="/about" component={AboutPage} />} />
+            <Route path="/contacts" component={ContactsPage} />} />
+            <Route path="/blog" component={BlogPage} />} />
             <PrivateRoute path="/profile" component={ProfilePage} redirectPath={'/'} exact />
-            <PrivateRoute path="/profile/animals" component={ProfilePage} redirectPath={'/'} exact />
+            <PrivateRoute path="/profile/animals" component={ProfileAnimalsPage} redirectPath={'/'} exact />
             <PrivateRoute path="/profile/add" component={ProfileAddAnimalPage} redirectPath={'/'} exact />
             <Route exact path="/" component={Home} />
             <Route path="*" component={NotFound} />
